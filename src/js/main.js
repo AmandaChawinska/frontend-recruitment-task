d54721fa746abc429ce661da2bba8ca391463c92
Amanda Chawinska <amanda.chawinska@spoko.pl>
2022-09-11 21:56:40 +0200

    added counter and reset button

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+console.log("Witam serdecznie :)");
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
@@ -33,3 +35,32 @@ function closeModal(modal) {
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+let counter= 0;
+
+const spanCounter = document.querySelector(".modal-counter");
+const button = document.querySelector(".container__button");
+const buttonReset = document.querySelector(".container__reset");
+
+
+function buttonClick() {
+    counter++;
+    spanCounter.textContent = counter;
+    if (counter >=5){
+        button.removeEventListener('click', buttonClick);
+        const resetButton = document.querySelector(".container__reset");
+        resetButton.classList.remove(".container__reset");
+        
+    }
+}
+
+function resetFunction() {
+    counter--;
+    spanCounter.textContent = 0;
+    const resetButton=document.querySelector(".container__reset");
+    resetButton.classList.add(".container__reset");
+}
+
+buttonReset.addEventListener('click', resetFunction);
+button.addEventListener('click', buttonClick);
+
